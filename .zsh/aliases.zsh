72e6ca2e9e28f395f263ad9ac462f8ed67978a04
LatikDesu <evga.abrosimov@gmail.com>
2024-07-08 20:45:30 +0300

    update

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -9,12 +9,7 @@ alias ap="ansible-playbook"
 
 alias code="open -a 'Visual Studio Code'"
 
-alias gprj="cd $HOME/Projects"
-alias gcs="cd $HOME/Projects/christianlempa/cheat-sheets"
-alias gbp="cd $HOME/Projects/christianlempa/boilerplates"
-
 alias ls="eza --icons --group-directories-first"
 alias ll="eza --icons --group-directories-first -l"
 #alias grep='grep --color'
 
-alias prx="ssh root@prx-prod-2.home.clcreative.de"
