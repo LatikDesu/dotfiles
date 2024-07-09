2efe653308b1d38cb13f85a289fe0d3c0d7f069c
LatikDesu <evga.abrosimov@gmail.com>
2024-07-09 11:32:03 +0300

    aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,5 +11,55 @@ alias code="open -a 'Visual Studio Code'"
 
 alias ls="eza --icons --group-directories-first"
 alias ll="eza --icons --group-directories-first -l"
-#alias grep='grep --color'
+alias lla="eza --icons --group-directories-first -la"
 
+# DOCKER - All Docker commands start with "d" AND Docker Compose commands start with "dc"
+alias dstop='sudo docker stop $(sudo docker ps -a -q)' # usage: dstop container_name
+alias dstopall='sudo docker stop $(sudo docker ps -aq)' # stop all containers
+alias drm='sudo docker rm $(sudo docker ps -a -q)' # usage: drm container_name
+alias dexec='sudo docker exec -ti' # usage: dexec container_name (to access container terminal)
+alias dps='sudo docker ps -a' # running docker processes
+alias dpss='sudo docker ps -a --format "table {{.Names}}\t{{.State}}\t{{.Status}}\t{{.Image}}" | (sed -u 1q; sort)' # running docker processes as nicer table
+
+alias dp600='sudo chown -R root:root $HOME/docker_data/secrets ; sudo chmod -R 600 $HOME/docker_data/secrets ; sudo chown -R root:root $HOME/docker_data/.env ; sudo chmod -R 600 $HOME/docker_data/.env' # re-lock permissions
+alias dp777='sudo chown -R $USER:$USER $HOME/docker_data/secrets ; sudo chmod -R 777 $HOME/docker_data/secrets ; sudo chown -R $USER:$USER $HOME/docker_data/.env ; sudo chmod -R 777 $HOME/docker_data/.env' # open permissions for editing
+
+# DOCKER COMPOSE
+alias dcrun='sudo docker-compose -f'
+
+
+# NAVIGATION
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# SYSTEMD START, STOP AND RESTART
+alias ctlreload='sudo systemctl daemon-reload'
+alias ctlstart='sudo systemctl start'
+alias ctlstop='sudo systemctl stop'
+alias ctlrestart='sudo systemctl restart'
+alias ctlstatus='sudo systemctl status'
+alias ctlenable='sudo systemctl enable'
+alias ctldisable='sudo systemctl disable'
+alias ctlactive='sudo systemctl is-active'
+
+alias sshstart='ctlstart ssh'
+alias sshstop='ctlstop ssh'
+alias sshrestart='ctlrestart ssh'
+alias sshstatus='ctlstatus ssh'
+
+# INSTALLATION AND UPGRADE
+alias update='sudo apt-get update'
+alias upgrade='sudo apt-get update && sudo apt-get upgrade'
+alias install='sudo apt-get install'
+alias finstall='sudo apt-get -f install'
+alias rinstall='sudo apt-get -f install --reinstall'
+alias uninstall='sudo apt-get remove'
+alias search='sudo apt-cache search'
+
+# SHUTDOWN AND RESTART
+alias shutdown='sudo shutdown -h now'
+alias reboot='sudo reboot'
--- README.md
+++ README.md
@@ -4,14 +4,22 @@ This Repository Dotfiles contain my personal config files. Here you'll find conf
 ### Terminal and Application Icons with Nerd-Fonts
 To display icons in terminal or applications Fonts, I'm using Nerd-Fonts. I'm currently using the Hack Nerd Font Mono in terminal applications, and Hack Nerd Font in applications.
 
+If need Helix Editor
 ```bash
-sudo apt-get install zsh eza direnv yadm
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
+```
+
+```bash
+sudo apt-get install zsh eza direnv yadm neofetch
 sudo curl -sS https://starship.rs/install.sh | sh
 yadm clone https://github.com/LatikDesu/dotfiles.git
 ```
 ```bash
 chsh -s $(which zsh)
 ```
+
 
 ![image](https://github.com/LatikDesu/dotfiles/assets/122733866/fe2f284d-0b69-4b32-9f91-9481642b21df)
 
